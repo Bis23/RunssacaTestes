da34b277daf1874ef83b4f8e57e2463c82a15036
Bianca Santos <104869999+Bis23@users.noreply.github.com>
2025-04-26 16:24:26 -0300

    Delete Tutorial.cs

--- Tutorial.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class Tutorial : MonoBehaviour
-{
-    public void loadBack() {
-        SceneManager.LoadScene("MainMenu");
-    }
-}
